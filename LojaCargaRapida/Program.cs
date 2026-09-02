cbb4d60896b8f6e13f487eef5793aed330fbdc6c
Thiago <thiroma9@gmail.com>
2026-09-02 15:12:09 -0300

    ajutes: Codificando o Programa.cs

--- LojaCargaRapida/Program.cs
+++ LojaCargaRapida/Program.cs
@@ -1,2 +1,30 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using LojaCargaRapida;
 
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("=== SISTEMA DE RECARGA===");
+Console.WriteLine();
+
+// 1. Instanciando o objeto da classe
+Estacao carga = new Estacao();
+
+// 2. Leitura dos dados e atribuição ao objeto
+Console.Write("Digite o nome do motorista: ");
+carga.Motorista = Console.ReadLine();
+
+Console.Write("Capacidade total da bateria (kWh): ");
+carga.CapacidadeKm  = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Carga atual da bateria (%): ");
+carga.PorcentagemAtual = double.Parse(Console.ReadLine());
+
+Console.Write("Potência do carregador (kW): ");
+carga.PotenciaKm  = double.Parse(Console.ReadLine());
+
+// 3. Exibição utilizando os métodos do objeto instanciado
+Console.WriteLine("\n--- COMPROVANTE DE SESSÃO ---");
+Console.WriteLine($"Cliente: {carga .Motorista.Trim().ToUpper()}");
+Console.WriteLine($"Carga Faltante: {carga.CalcularKWNecessarios ():F1} kWh");
+Console.WriteLine($"Tempo Estimado: {carga.CalcularTempoHoras():F2} hora(s)");
+Console.WriteLine($"Valor a Pagar: R$ {carga.CalcularValor ():N2}");
+Console.WriteLine($"Sessão Super-Rápida? {carga.CarregamentoRapido ()}");
