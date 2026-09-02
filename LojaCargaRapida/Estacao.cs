d8226923b1b814e8d5fe8bef7deec7fdde28b3a1
Thiago <thiroma9@gmail.com>
2026-09-02 14:53:33 -0300

    feat: Crinando a classe estacão carga

--- LojaCargaRapida/Estacao.cs
+++ LojaCargaRapida/Estacao.cs
@@ -1,12 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace LojaCargaRapida
 {
-    internal class Estacao
+     class Estacao
     {
+        //PROPRIEDADES()
+        public string Motorista { get; set; }
+
+        public double CapacidadeKm { get; set; }
+
+        public double PorcentagemAtual { get; set; }
+
+        public double PotenciaKm { get; set; }
+
+
+        //metodo
+
+        public double CalcularKWNecessarios()
+        {
+            double PorcentagemFaltante = (100.00 - PorcentagemAtual) / 100.00;
+            return CapacidadeKm * PorcentagemFaltante;
+        }
+
+        //
+
+        public double CalcularTempoHoras()
+        {
+            return CalcularKWNecessarios() / PotenciaKm;
+
+        }
+
+        //
+        public double CalcularValor()
+        {
+            const double PRECO_POR_KWN = 2.50;
+            return CalcularKWNecessarios() * PRECO_POR_KWN;
+
+        }
+
+        // 
+        public bool CarregamentoRapido()
+        {
+            return (PotenciaKm >= 50.0) && (CalcularTempoHoras() < 1.0);
+        }
+
+
     }
 }
